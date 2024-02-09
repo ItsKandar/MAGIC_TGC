b61b95cd3b3b32e9e0328c320c17e3934bf9fbbc
ItsKandar <nmoreau15@myges.fr>
2024-02-09 17:49:49 +0100

    fixed cameras and UIs

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,104 @@
+// Make the buttons change the camera priorities
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button buttonBibliotheque;
+    public Button buttonTable;
+    public Button buttonQuitter;
+
+    public Button BackFromCloset;
+    public Button BackFromTable;
+
+    public Button BackFromOutside;
+    public Button ConfirmQuit;
+    
+    public GameObject TVUI;
+    public GameObject ClosetUI;
+    public GameObject TableUI;
+    public GameObject ExitUI;
+
+    public CinemachineVirtualCamera cameraTV;
+    public CinemachineVirtualCamera cameraCloset;
+    public CinemachineVirtualCamera cameraTable;
+    public CinemachineVirtualCamera cameraOutside;
+
+    void SwitchCamera(CinemachineVirtualCamera cameraToActivate)
+    {
+        Debug.Log("Switching to camera: " + cameraToActivate);
+
+        // Camera Closet
+        if (cameraToActivate == cameraCloset)
+        {   
+            DisableUIs(TVUI);
+            cameraCloset.Priority = (cameraToActivate == cameraCloset) ? 10 : 0;
+            EnableUIs(ClosetUI);
+            BackFromCloset.onClick.AddListener(() => SwitchCamera(cameraTV));
+        }
+
+        // Camera Table
+        if (cameraToActivate == cameraTable)
+        {
+            DisableUIs(TVUI);
+            cameraTable.Priority = (cameraToActivate == cameraTable) ? 10 : 0;
+            EnableUIs(TableUI);
+            BackFromTable.onClick.AddListener(() => SwitchCamera(cameraTV));
+        }
+
+        // Camera Outside
+        if (cameraToActivate == cameraOutside)
+        {
+            DisableUIs(TVUI);
+            cameraOutside.Priority = (cameraToActivate == cameraOutside) ? 10 : 0;
+            EnableUIs(ExitUI);
+            ConfirmQuit.onClick.AddListener(() => Application.Quit());
+            BackFromOutside.onClick.AddListener(() => SwitchCamera(cameraTV));
+        }
+
+        // Camera TV
+        
+        cameraTV.Priority = (cameraToActivate == cameraTV) ? 10 : 0;
+        if (cameraToActivate == cameraTV)
+        {
+            DisableUIs(TableUI);
+            DisableUIs(ClosetUI);
+            DisableUIs(ExitUI);
+            EnableUIs(TVUI);
+        }
+    }
+
+    void DisableUIs(GameObject UIToDisable)
+    {
+        UIToDisable.SetActive(false);
+    }
+
+    void EnableUIs(GameObject UItoEnable)
+    {
+        UItoEnable.SetActive(true);
+    }
+
+    void Start()
+    {
+        // Add listeners to the buttons
+        buttonBibliotheque.onClick.AddListener(() => SwitchCamera(cameraCloset));
+        buttonTable.onClick.AddListener(() => SwitchCamera(cameraTable));
+        buttonQuitter.onClick.AddListener(() => SwitchCamera(cameraOutside));
+
+        
+        // Disable UIs
+        ClosetUI.SetActive(false);
+        TableUI.SetActive(false);
+        ExitUI.SetActive(false);
+
+        // Enable TV UI
+        TVUI.SetActive(true);
+
+        // Start with the TV camera
+        SwitchCamera(cameraTV);
+    }
+}
